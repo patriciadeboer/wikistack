fc69dc1dda575f76e1b6d02ecd43709e05e571ff
Tao Wang <tao.wang6000@gmail.com>
2019-04-25 13:32:33 -0500

    other partial

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { Page } = require("../models");
 
 const {addPage}= require('./../views');
+
+function generateSlug (title) {
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
 
 router.get('/',(req,res,next)=>{
   res.send('got to GET /wiki/')
 });
 
-router.post('/',(req,res,next)=>{
-  res.send('got to POST /wiki/')
+router.post('/', async (req,res,next)=>{
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+  console.log(page)
+  // res.json(req.body)
+
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
 });
 
 router.get('/add',(req,res)=>{
